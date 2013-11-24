aaff1194d264b1720a1fdadc5f106a6561715b67
David Flores <dmousex@gmail.com>
2013-11-24 13:42:18 -0600

    change event listener

--- src/Drupal/Composer/Drupal/DrupalPlugin.php
+++ src/Drupal/Composer/Drupal/DrupalPlugin.php
@@ -7,7 +7,10 @@ use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Plugin\PluginEvents;
-use Composer\Plugin\PreFileDownloadEvent;
+use Composer\Plugin\CommandEvent;
+use Composer\Util\RemoteFilesystem;
+use Composer\Json\JsonFile;
+use Composer\Installer;
 
 class DrupalPlugin implements PluginInterface, EventSubscriberInterface {
 
@@ -18,15 +21,30 @@ class DrupalPlugin implements PluginInterface, EventSubscriberInterface {
 
   public static function getSubscribedEvents(){
     return array(
-      PluginEvents::PRE_FILE_DOWNLOAD => array(
-          array('onPreFileDownload', 0)
+      PluginEvents::COMMAND => array(
+          array('mergeComposers', 0)
       ),
     );
   }
 
-  public function onPreFileDownload(PreFileDownloadEvent $event) {
-    $protocol = parse_url($event->getProcessedUrl(), PHP_URL_SCHEME);
-    print_r( $protocol);
+  public function mergeComposers(CommandEvent $event) {
+    //$locker = $this->composer->getLocker();
+
+
+    /*$file = new JsonFile('sites/default/files/composer.json', new RemoteFilesystem($this->io));
+    $file->validateSchema(JsonFile::LAX_SCHEMA);
+    $localConfig = $file->read();*/
+
+    $factory = new \Composer\Factory();
+    $composer = $factory->createComposer($this->io, './sites/default/files/composer.json');
+    $localRepo = $composer->getRepositoryManager();
+
+    $localRepo->reload();
+
+    //$locker->getLockData();
+
+    //print_r ($locker->getLockData());
+    exit(0);
   }
 
 }
