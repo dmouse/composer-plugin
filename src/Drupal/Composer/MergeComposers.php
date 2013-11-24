30c13b22aefbdcf13ca5745e4a9364d5b6d85bb3
David Flores <dmousex@gmail.com>
2013-11-23 22:34:22 -0600

    init commit

---
+++ src/Drupal/Composer/MergeComposers.php
@@ -0,0 +1,30 @@
+<?php
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PreFileDownloadEvent;
+
+class MergeComposers implements PluginInterface, EventSubscriberInterface {
+
+  public function activate(Composer $composer, IOInterface $io){
+    $this->composer = $composer;
+    $this->io = $io;
+  }
+
+  public static function getSubscribedEvents(){
+    return array(
+      PluginEvents::PRE_FILE_DOWNLOAD => array(
+          array('onPreFileDownload', 0)
+      ),
+    );
+  }
+
+  public function onPreFileDownload(PreFileDownloadEvent $event) {
+    $protocol = parse_url($event->getProcessedUrl(), PHP_URL_SCHEME);
+    print_r( $protocol);
+  }
+
+}
