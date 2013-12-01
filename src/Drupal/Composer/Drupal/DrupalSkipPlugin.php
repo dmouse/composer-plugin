8274049181dec779e9c1b1fd530103c194a01984
David Flores <dmousex@gmail.com>
2013-12-01 10:10:58 -0600

    plugin on PRE_FILE_DOWNLOAD event

--- src/Drupal/Composer/Drupal/DrupalSkipPlugin.php
+++ src/Drupal/Composer/Drupal/DrupalSkipPlugin.php
@@ -13,7 +13,7 @@ use Composer\Config\JsonConfigSource;
 use Composer\Json\JsonFile;
 use Composer\Installer;
 
-class DrupalPlugin implements PluginInterface, EventSubscriberInterface {
+class DrupalSkipPlugin implements PluginInterface, EventSubscriberInterface {
 
   public function activate(Composer $composer, IOInterface $io){
     $this->composer = $composer;
@@ -22,26 +22,14 @@ class DrupalPlugin implements PluginInterface, EventSubscriberInterface {
 
   public static function getSubscribedEvents(){
     return array(
-      PluginEvents::COMMAND => array(
-          array('mergeComposers', 0)
+      PluginEvents::PRE_FILE_DOWNLOAD => array(
+          array('drupalSkip', 0)
       ),
     );
   }
 
-  public function mergeComposers(CommandEvent $event) {
-
-    $file = new JsonFile('/home/dmouse/public_html/vms/RpzAUX/www/sample/composer.json');
-    $drupal_composer_json = $file->read();
-
-    print_r($drupal_composer_json);
-
-    $file = new JsonFile('/home/dmouse/public_html/vms/RpzAUX/www/sample/composer.lock');
-    $drupal_composer_lock = $file->read();
-
-    /*foreach ($drupal_composer_lock['packages'] as $key => $value) {
-
-    }*/
-
+  public function drupalSkip(PreFileDownloadEvent $event){
+    print_r($event);
   }
 
 }
